95611dc88349e16a83046faf3b5a1812bdb9ae57
michael vitello <michael.vitello@gmail.com>
2018-06-16 10:59:47 +0200

    wip exercise - no commom folder

--- github-followers/src/app/github-followers.service.ts
+++ github-followers/src/app/github-followers.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class GithubFollowersService {
+export class GithubFollowersService extends DataService {
 
   constructor() { }
 }
